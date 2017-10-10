1445bb602f212daaf931223babd04bb15b7c5265
Lee <lees13@wit.edu>
2017-10-09 22:40:00 -0400

    Add string reverse

---
+++ src/ReverseString.java
@@ -0,0 +1,31 @@
+public class ReverseString {
+	
+	private String reverseLoop(String str) {
+		String res = "";
+		for(int i = str.length() - 1; i >= 0; i--)
+			res += str.charAt(i);
+		return res;
+	}
+	
+	private String reverseRec(String str) {
+		if (str.length() < 2) {
+            return str;
+        }
+
+        return reverseRec(str.substring(1)) + str.charAt(0);
+        // rec(HELLO) = OLLE + H
+        // rec(ELLO)  = OLL	+ E
+        // rec(LLO)   = OL	+ L
+        // rec(LO) 	  = O	+ L
+        // rec(O) 	  = O
+	}
+	
+	public static void main(String[] args) {
+		String str = "Hello";
+		ReverseString rev = new ReverseString();
+		System.out.println(str);
+		System.out.println(rev.reverseLoop(str));
+		System.out.println(rev.reverseRec(str));
+		
+	}
+}
